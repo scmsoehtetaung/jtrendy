5afb862c69668bd4e8107c0d43268e8202050b0a
khinmyohtet2 <scm.khinmyohtet@gmail.com>
2019-06-13 10:23:40 +0630

    upload song show song category user list

--- database/migrations/2019_06_13_031028_create_like_song_table.php
+++ database/migrations/2019_06_13_031028_create_like_song_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateLikeSongTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('like_song', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('phone_number')->unique();
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->integer('user_type');
-            $table->rememberToken();
+            $table->integer('song_id')->nullable();
+            $table->integer('user_id')->nullable();
             $table->timestamps();
         });
     }
@@ -32,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('like_song');
     }
 }
