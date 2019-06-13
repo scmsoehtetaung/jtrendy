88aad457c4917961874114352e313d606f7906e9
ソテタウン <46985990+scmsoehtetaung@users.noreply.github.com>
2019-06-13 10:47:42 +0630

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('phone_number')->unique();
             $table->string('email')->unique();
+            $table->string('phone_number')->unique();
             $table->string('password');
             $table->integer('user_type');
             $table->rememberToken();
